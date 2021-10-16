8a80fc91ef67319ad537cb8be83d027c4aba40f5
Ronald Vyhmeister <ronaldvm33@yahoo.com>
2021-10-16 15:28:04 -0300

    Use model to get meetup data

--- meetups/views.py
+++ meetups/views.py
@@ -1,34 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Meetup
 
 # Create your views here.
 
 
 def index(request):
-    meetups = [
-        {
-            'title': 'A first meetup',
-            'location': 'New York',
-            'slug': 'a-first-meetup'
-        },
-        {
-            'title': 'A second meetup',
-            'location': 'Paris',
-            'slug': 'a-second-meetup'
-        }
-    ]
+    meetups = Meetup.objects.all()
     return render(request, 'meetups/index.html', {
-        'show_meetups': True,
         'meetups': meetups
     })
 
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = {
-        'title': 'A First Meetup',
-        'description': 'This is the first meetup!'
-    }
-    return render(request, 'meetups/meetup-details.html', {
-        'meetup_title': selected_meetup['title'],
-        'meetup_description': selected_meetup['description']
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description
+        })
+    except Meetup.DoesNotExist:
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False
+        })
