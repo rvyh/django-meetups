a50556cf691851822ad306c5a1c29f393d0a71a5
Ronald Vyhmeister <ronaldvm33@yahoo.com>
2021-10-17 19:27:09 -0300

    Add image upload from admin panel

--- meetups/models.py
+++ meetups/models.py
@@ -7,3 +7,4 @@ class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+    image = models.ImageField(upload_to='images')
