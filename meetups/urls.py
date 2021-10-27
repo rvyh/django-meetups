5175ef96dfe7bd5c14c3b0a1302015ca8397bde3
Ronald Vyhmeister <ronaldvm33@yahoo.com>
2021-10-27 17:49:29 -0300

    Add organizer email to confirmation page

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # domain.com/meetups
     path('meetups/', views.index, name='all-meetups'),
-    path('meetups/success', views.confirm_registration, name='confirm-registration'),
+    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
     # domain.com/meetups/<dynamic-path>
     path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-details')
 
