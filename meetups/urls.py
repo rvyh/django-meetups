76080974de85c7ed0d56035872b388a3b7c7465e
Ronald Vyhmeister <ronaldvm33@yahoo.com>
2021-10-27 17:17:47 -0300

    Add success page to registrations

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     # domain.com/meetups
     path('meetups/', views.index, name='all-meetups'),
+    path('meetups/success', views.confirm_registration, name='confirm-registration'),
     # domain.com/meetups/<dynamic-path>
     path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-details')
+
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Meetup
+from django.shortcuts import render, redirect
+from .models import Meetup, Participant
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -14,11 +15,26 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == "GET":
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                user_email = registration_form.cleaned_data['email']
+                participant, _ = Participant.objects.get_or_create(email=user_email)
+                selected_meetup.participants.add(participant)
+                return redirect('confirm-registration')
+
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': True,
-            'meetup': selected_meetup
+            'meetup': selected_meetup,
+            'form': registration_form
         })
     except Meetup.DoesNotExist:
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': False
         })
+
+
+def confirm_registration(request):
+    return render(request, 'meetups/registration-success.html')
